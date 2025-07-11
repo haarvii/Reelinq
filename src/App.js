2268c2f239fdef11083376cb4562de448e2d4646
Harsh Doshi <harshdoshi789@gmail.com>
2025-07-11 16:54:42 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
           <Route path="notifications" element={<Notifications />} />
           <Route path="history" element={<History />} />
           <Route path="brands" element={<Brands />} />
+          
         </Route>
       </Routes>
     </Router>
